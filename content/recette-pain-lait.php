d180eae5e6e0daa21d3dab65f9c4f8e41f56a79b
Encre Informatique <encreinformatique@users.noreply.github.com>
2024-05-30 21:20:40 +0000

    new : icones, début de recettes et CSS

--- content/recette-pain-lait.php
+++ content/recette-pain-lait.php
@@ -1,6 +1,6 @@
-<h1 class="text-3xl font-bold mx-3 my-3">Pain de mie au lait</h1>
+<h1>Pain de mie au lait</h1>
 
-<h2 class="mx-3">Ingrédients</h2>
+<h2>Ingrédients</h2>
 <ul class="mx-3 my-1">
     <li>500 g de farine</li>
     <li>200 ml de lait</li>
@@ -9,12 +9,16 @@
     <li>1 cuillière à café de sel</li>
 </ul>
 
-<p class="mx-3">Pour hydrater la levure</p>
+<p>Pour hydrater la levure</p>
 <ul class="mx-3">
     <li>50 ml d'eau (pour hydrater la levure)</li>
     <li>25 g de levure fraîche</li>
 </ul>
 
-<h2 class="mx-3">Préparation</h2>
-<p class="mx-3">Hydrater la levure.</p>
-<p class="mx-3">Mélanger les ingrédients sauf le sel.</p>
+<h2>Préparation</h2>
+<p>Hydrater la levure.</p>
+<p>Mélanger les ingrédients sauf le sel.</p>
+<p>Ajouter la levure.</p>
+<p>Ajouter le sel après avoir mélanger la levure, ou ajouter le sel au tout début et bien mélanger<sup>1<sup>.</p>
+
+<p><sup>1</sup>  Le but est de ne pas tuer la levure. Je trouve que le pain gonfle moins facilement.</p>
--- src/header.php
+++ src/header.php
@@ -3,12 +3,24 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
+  <link rel="manifest" href="./site.webmanifest">
   <link href="./output.css" rel="stylesheet">
+  <style>
+    h1 {font-size:1.875rem;font-weight:700;line-height:2.25rem;margin:0.75rem 2rem;}
+    h2 {font-size:1.275rem;font-weight:700;line-height:1.75rem;margin:0.5rem 2rem;}
+    p {margin:0.25rem 2rem;}
+    a {color: rgb(96 165 250 / var(--tw-text-opacity));}
+    a:hover {color: rgb(219 39 119 / var(--tw-text-opacity));}
+    footer {margin:0.25rem 2rem;}
+  </style>
 </head>
 <body>
 
 <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-  <a href="./" class="-m-1.5 p-1.5 text-3xl font-bold">
+  <a href="./" class="-m-1.5 p-1.5 text-3xl font-bold hover:text-red-600">
     Mes Recettes
   </a>
 </nav>
