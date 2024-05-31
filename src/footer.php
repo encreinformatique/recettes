4bf451c252bf7480b3f1571dfc2cb2af826bff8e
Encre Informatique <encreinformatique@users.noreply.github.com>
2024-05-31 19:33:29 +0200

    Update footer.php

--- src/footer.php
+++ src/footer.php
@@ -1,8 +1,8 @@
 <footer>
-    <p class="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
+    <p class="mt-8 text-xs leading-5 md:order-1 md:mt-0">
         Si vous voyez une erreur, vous pouvez le commenter <a href="https://github.com/encreinformatique/static-test/issues">sur Github</a>.
     </p>
-    <p class="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">&copy; 2024 Encre Informatique - Mes Recettes sont fournies sans garantie, c'est vous qui cuisinez. :)</p>
+    <p class="mt-8 text-xs leading-5 md:order-1 md:mt-0">&copy; 2024 Encre Informatique - Mes Recettes sont fournies sans garantie, c'est vous qui cuisinez. :)</p>
 </footer>
 </body>
 <html>
