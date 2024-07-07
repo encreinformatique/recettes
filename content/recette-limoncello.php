d70ae7bd3b34c08f1ce72e0eb59dd6e506505305
Encre Informatique <encreinformatique@users.noreply.github.com>
2024-07-07 16:00:20 +0200

    Actualizar recette-limoncello.php

--- content/recette-limoncello.php
+++ content/recette-limoncello.php
@@ -2,4 +2,12 @@
 
 <p>Outre les <a href="./#bieres">bières</a>, le Limoncello est amusant à produire. C'est beaucoup plus simple même si le processus de maduration est tout aussi long.</p>
 
+<ul>
+<li>macération du zeste de citron dans l'alcool</li>
+<li>mélanger avec l'almibar</li>
+<li>macération pendant 15 jours (confirmer avec les notes)</li>
+<li>filtrer et embouteiller</li>
+<li>mettre en garde pendant 2 à 3 semaines</li>
+</ul>
+
 <p>Recette complète à recopier des notes.</p>
