ee8f61fce6c75b1a8f51a6d662c1293e48fe99bc
Encre Informatique <encreinformatique@users.noreply.github.com>
2024-09-12 19:24:32 +0200

    Create recette-pancake.php

---
+++ content/recette-pancake.php
@@ -0,0 +1,12 @@
+<h1>Pancake</h1>
+
+<h2>Ingrédients</h2>
+<ul class="mx-3 my-1">
+    <li>125 g de farine</li>
+    <li>150 ml de lait</li>
+    <li>2 oeufs</li>
+    <li>5 g de levure chimique</li>
+    <li>1 cuillière à café d'arôme de vanille</li>
+    <li>50 g de sucre</li>
+    <li>1 cuillière à café de sel</li>
+</ul>
